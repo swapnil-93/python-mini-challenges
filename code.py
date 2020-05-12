4470f6f4e32bbfb405f8c5283612feaf6f977c8c
swapnil-93 <swapnil-93@users.noreply.github.com>
2020-05-12 20:51:39 +0530

    Code file created.

---
+++ code.py
@@ -0,0 +1,93 @@
+# --------------
+#Code starts here
+
+#Function to check for palindrome
+def palindrome_check(num):
+  num=str(num)
+  return (num[::-1]==num)
+
+#Function to find the smallest palindrome
+def palindrome(num):
+    while(1):
+        num=num+1
+        if palindrome_check(num):
+            return num
+        
+palindrome(11)
+#Code ends here        
+
+
+# --------------
+#Code starts here
+
+#Function to find anagram of one word in another
+
+def a_scramble(str_1,str_2):
+    result=True
+    for i in (str_2.lower()):
+        if i not in (str_1.lower()):
+            result=False
+            break
+        str_1=str_1.replace(i,'',1) #Removing the letters from str_1 that are already checked
+    
+    return (result)
+
+#Code ends here
+
+
+# --------------
+#Importing header files
+from math import sqrt
+
+#Code starts here
+
+#Function to check for perfect square 
+def is_perfect_square(x):
+ 
+    s = sqrt(x)
+    return (int(s)*int(s) == x) 
+ 
+#Function to check for fibonacci number
+def check_fib(num):
+    if is_perfect_square((5*num*num) + 4) or is_perfect_square((5*num*num) - 4): #Formula for checking fibonacci number
+        return True
+    
+    return False     
+
+#Code ends here
+
+
+# --------------
+#Code starts here
+
+#Function to compress string
+def compress(word):
+    word=word.lower()
+    mist=[]
+    l=0
+    while(l<len(word)):
+        m=word[l]
+        j=0
+        while(l<len(word) and word[l]==m):
+                 j=j+1
+                 l=l+1    
+
+        mist.append(m)
+        mist.append(str(j))
+    
+    return ''.join(mist)
+
+#Code ends here
+
+
+# --------------
+#Code starts here
+
+#Function to check existence of k distinct characters in string
+def k_distinct(string,k):
+    s_list=(set(string.lower()))
+    return len(s_list)>=k
+
+#Code ends here
+
+
